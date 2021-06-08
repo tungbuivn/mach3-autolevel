789634cd4b2acf5535d125632aa6592e9d736eca
Tung Bui <tungbuivn@gmail.com>
2021-06-08 16:13:17 +0700

    update

--- ger.js
+++ ger.js
@@ -36,6 +36,7 @@ function applyTpl(){
     var bottomLayer=get("BottomLayer");
     var drill=get("_PTH");
 return `
+set_sys units in
 new
 open_gerber ${boardOutline} -outname cutout
 open_gerber ${bottomLayer} -outname bottom_layer
