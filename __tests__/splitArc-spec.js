746728edf122c57eb096c8a4adc32253b3b5b77c
Tung Bui <tungbuivn@gmail.com>
2021-06-30 18:16:01 +0700

    update

--- __tests__/splitArc-spec.js
+++ __tests__/splitArc-spec.js
@@ -6,5 +6,5 @@ test("splitArc test", () => {
       { center: { x: 1, y: 0 }, start: { x: 0, y: 0 }, end: { x: 1, y: 1 } },
       [{ p1: { x: 1, y: 0 }, p2: { x: 0, y: 1 } }]
     );
-  expect(rs);
+  expect(rs.length).toBe(2);
 });
